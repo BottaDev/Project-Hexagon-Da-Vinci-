a8fa4871282c3f4edfe6d8b3400075176d6ab2fb
BottaDev <fedebotta@gmail.com>
2019-11-22 21:55:55 -0300

    Menu Tutorial

---
+++ Assets/Scripts/ChangeColorUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColorUI : MonoBehaviour
+{
+    public Image image;
+    float timeLeft;
+    Color targetColor;
+
+    void Update()
+    {
+        if (timeLeft <= Time.deltaTime)
+        {
+
+            image.color = targetColor;
+
+            targetColor = new Color(Random.value, Random.value, Random.value);
+
+            timeLeft = 1f;
+        }
+        else
+        {
+
+            image.color = Color.Lerp(image.color, targetColor, Time.deltaTime / timeLeft);
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
